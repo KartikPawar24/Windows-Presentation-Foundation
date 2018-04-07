7e54b089e8fce0b5937484b977a126ec566c7b65
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2018-04-08 00:24:13 +0530

    Add files via upload
    In this it contains the WPF app which changes its color when clicked on a button

---
+++ Coloured label text/Coloured label text/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Coloured_label_text
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnred_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked Red");
+            lblblue.Content = String.Empty;
+            lblgreen.Content = String.Empty;
+            lblviolet.Content = String.Empty;
+        }
+
+        private void btnblue_Click(object sender, RoutedEventArgs e)
+        {
+            lblred.Content = String.Empty;
+            MessageBox.Show("You clicked Blue");
+            lblgreen.Content = String.Empty;
+            lblviolet.Content = String.Empty;
+        }
+
+        private void btngreen_Click(object sender, RoutedEventArgs e)
+        {
+            lblred.Content = String.Empty; ;
+            lblblue.Content = String.Empty;
+            lblgreen.Content = "You clicked Green";
+            lblviolet.Content = String.Empty;
+        }
+
+        private void btnviolet_Click(object sender, RoutedEventArgs e)
+        {
+            lblred.Content = String.Empty; ;
+            lblblue.Content = String.Empty;
+            lblgreen.Content = String.Empty;
+            lblviolet.Content = "You clicked Violet";
+        }
+    }
+}
