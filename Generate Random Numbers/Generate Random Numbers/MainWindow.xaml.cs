cdb8ec478bba65ddffa77b1ddd0b0773e70cac0a
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2018-04-08 00:27:45 +0530

    Add files via upload
    This app is used to generate a random number

---
+++ Generate Random Numbers/Generate Random Numbers/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Generate_Random_Numbers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnran_Click(object sender, RoutedEventArgs e)
+        {
+            int data = Convert.ToInt16(txtnum.Text);
+            Random rannum = new Random();
+            int res = rannum.Next(1, data);
+            txtres.Text = res.ToString();
+        }
+
+        private void btnclr_Click(object sender, RoutedEventArgs e)
+        {
+            txtnum.Text = String.Empty;
+            txtres.Text = String.Empty;
+        }
+    }
+}
