63642488f12eb364d059777bba9d1c3c568f4289
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2018-04-08 00:14:09 +0530

    Add files via upload
    This is an app which calculates the Arithmetic Operations using WPF

---
+++ Arithmtic Operations using WPF/Arithmtic Operations using WPF/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arithmtic_Operations_using_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnclk.Click += Btnclk_Click;
+        }
+
+        private void Btnclk_Click(object sender, RoutedEventArgs e)
+        {
+            lblname.Content = "Thank You";
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtnum1.Text);
+            double b = Convert.ToDouble(txtnum2.Text);
+            double c = a + b;
+            txtres.Text = c.ToString();
+        }
+
+        private void btnsub_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtnum1.Text);
+            double b = Convert.ToDouble(txtnum2.Text);
+            double c = a - b;
+            txtres.Text = c.ToString();
+        }
+
+        private void btnmul_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtnum1.Text);
+            double b = Convert.ToDouble(txtnum2.Text);
+            double c = a * b;
+            txtres.Text = c.ToString();
+        }
+
+        private void btndiv_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtnum1.Text);
+            double b = Convert.ToDouble(txtnum2.Text);
+            double c = a / b;
+            txtres.Text = c.ToString();
+        }
+
+        private void btnrem_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtnum1.Text);
+            double b = Convert.ToDouble(txtnum2.Text);
+            double c = a % b;
+            txtres.Text = c.ToString();
+        }
+
+        private void btnclr_Click(object sender, RoutedEventArgs e)
+        {
+            txtnum1.Text = "";
+            txtnum2.Text = "";
+            txtres.Text = "";
+
+        }
+    }
+}
